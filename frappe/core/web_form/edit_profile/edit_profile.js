ce7e2deab88427eede75c64ebfed172bff54e1ce
charlie9526 <asansandeepa@gmail.com>
2020-07-02 21:21:02 +0530

    Completed Account Requirement Setting change still to be updated via DB
    Signed-off-by: charlie9526 <asansandeepa@gmail.com>

--- frappe/core/web_form/edit_profile/edit_profile.js
+++ frappe/core/web_form/edit_profile/edit_profile.js
@@ -1,3 +1,14 @@
 frappe.ready(function() {
 	// bind events here
+	var div_reset_password_button = document.createElement("div");
+	div_reset_password_button.className = "page-header d-flex align-items-center";
+	var reset_password_button = document.createElement("a"); 
+	reset_password_button.innerHTML = "Reset Password"; 
+	reset_password_button.className = "btn btn-primary ml-auto";
+	reset_password_button.href = "update-password"
+	
+	var parent_node =  document.getElementsByClassName("page-breadcrumbs")[0];
+	div_reset_password_button.append(reset_password_button)
+	parent_node.after(div_reset_password_button);
 })
+
